196e85a03b9e3f43dfe157ec6ae9e06f1dc75ddf
Piyush-Srivastava <piyush061996@gmail.com>
2020-05-06 18:46:49 +0530

    Added sign up popup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import FormPage from "./components/layout/FormPage";
 
 function App() {
   return (
     <div className="App">
       <h1>Social Media App</h1>
+      <FormPage></FormPage>
     </div>
   );
 }
