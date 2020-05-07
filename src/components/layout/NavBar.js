b162ad61a44e2fa8241ea7b4c7bcb776d009b5c3
Piyush-Srivastava <piyush061996@gmail.com>
2020-05-07 20:37:30 +0530

    Added buttons in navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,6 +4,12 @@ function NavBar() {
   return (
     <nav className="navbar bg-dark">
       <h1>Social Media App</h1>
+      <button type="button" class="btn btn-primary ">
+        SignUp
+      </button>
+      <button type="button" class="btn btn-light ">
+        SignIn
+      </button>
     </nav>
   );
 }
